47099d5066e6c0d1e4c5333c8d4cd6174e7f3c35
Bajdzis <rafal612b@wp.pl>
2019-08-31 11:14:25 +0200

    migrate BEMBlock component

---
+++ src/decorators/BEMBlock.tsx
@@ -0,0 +1,25 @@
+import * as PropTypes from 'prop-types'; 
+import { convertBemValueToArray} from '../helpers';
+import { BEMNode } from './BEMNode';
+
+interface BEMBlockContext {
+    BEM_BlockNames: string[];
+}
+
+export class BEMBlock extends BEMNode {
+
+    static childContextTypes: {[key in keyof BEMBlockContext]: PropTypes.Requireable<any>} = {
+        BEM_BlockNames: PropTypes.arrayOf(PropTypes.string),
+    };
+
+    getChildContext(): BEMBlockContext {
+        return {
+            BEM_BlockNames: this.getNames(),
+        };
+    }
+
+    getNames(): string[] {
+        return convertBemValueToArray(this.props.bemName);
+    }
+
+}
